ef73be926efa95937385bd2eb494270dd66353d4
Mykola Mosiienko <koljamos@gmail.com>
2025-12-02 17:09:40 +0000

    Implemented task

--- app/main.py
+++ app/main.py
@@ -5,26 +5,23 @@ class Animal:
 
     alive = []
 
-    def __init__(self, name: str) -> None:
-        self.health = 100
+    def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
         self.hidden = False
+        self.health = health
         Animal.alive.append(self)
 
     def __repr__(self) -> str:
-        anim = {}
-        anim["Name"] = self.name
-        anim["Health"] = self.health
-        anim["Hidden"] = self.hidden
-        return f"{anim}"
+        return (f"{{Name: {self.name}, Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
 
 class Carnivore(Animal):
-    def bite(self, herby: Herbivore) -> None:
-        if isinstance(herby, Herbivore) and not herby.hidden:
-            herby.health -= 50
-            if herby.health <= 0 and herby in Animal.alive:
-                Animal.alive.remove(herby)
+    def bite(self, herb: Herbivore) -> None:
+        if isinstance(herb, Herbivore) and not herb.hidden:
+            herb.health -= 50
+            if herb.health <= 0 and herb in Animal.alive:
+                Animal.alive.remove(herb)
 
 
 class Herbivore(Animal):
